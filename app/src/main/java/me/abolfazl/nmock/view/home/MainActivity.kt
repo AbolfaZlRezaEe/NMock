c19fc59a050db4ed958de0cbe4de5bf2cfa06b4c
AbolFazL RezaEi <abolfazlrezaei.of@gmail.com>
2022-04-29 14:24:58 +0430

    initial commit

---
+++ app/src/main/java/me/abolfazl/nmock/view/home/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.abolfazl.nmock.view.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.abolfazl.nmock.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
